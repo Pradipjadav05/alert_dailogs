d63f93a2418b03a657063ed6fd1c29daf50d3218
Pradipjadav05 <pjadav669@rku.ac.in>
2023-09-03 12:17:10 +0530

    first commit

---
+++ lib/alert_dailogs.dart
@@ -0,0 +1,60 @@
+library alert_dailogs;
+
+import 'package:flutter/material.dart';
+
+enum DialogType { alert, warning, confirmation }
+
+class CustomIconDialog extends StatelessWidget {
+  final DialogType dialogType;
+  final String title;
+  final String content;
+  final VoidCallback? onConfirm;
+
+  const CustomIconDialog(
+      {Key? key,
+        required this.dialogType,
+        required this.title,
+        required this.content,
+        this.onConfirm})
+      : super(key: key);
+
+  IconData _getIcon() {
+    switch (dialogType) {
+      case DialogType.alert:
+        return Icons.warning;
+      case DialogType.warning:
+        return Icons.error;
+      case DialogType.confirmation:
+        return Icons.question_answer;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Row(
+        children: <Widget>[
+          Icon(_getIcon()),
+          const SizedBox(width: 8), // Adjust the spacing as needed
+          Text(title),
+        ],
+      ),
+      content: Text(content),
+      actions: <Widget>[
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: const Text('Cancel'),
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+            onConfirm?.call();
+          },
+          child: const Text('OK'),
+        ),
+      ],
+    );
+  }
+}
